de13d33a7881a5537d0d0d204733c2c27d19a305
JuanDavidBallesteros <juan.ballesteros@u.icesi.edu.co>
2021-05-29 22:15:44 -0500

    tests diagrams

---
+++ test/model/TripTest.java
@@ -0,0 +1,19 @@
+package model;
+
+import org.junit.Test;
+
+public class TripTest {
+
+	private Trip trip;
+
+	private void setupScenary1() {
+		trip = new Trip();
+	}
+
+	@Test
+	public void testAddLuggage() throws Exception {
+
+	}
+
+	
+}
