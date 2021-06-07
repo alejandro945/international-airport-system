b44a847fb67dfbe7ed295db3afb88b060aff370c
Sinis2101 <sinisterra2101@gmail.com>
2021-06-07 13:39:16 -0500

    Airlines CRUD implemented.

--- src/controller/crud/AirlineController.java
+++ src/controller/crud/AirlineController.java
@@ -1,24 +1,33 @@
 package controller.crud;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXTextField;
 
+import controller.Constant;
 import controller.DashboardController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import model.Airline;
 import model.Airport;
 import route.Route;
 
-public class AirlineController {
+public class AirlineController implements Initializable {
     @FXML
     private Button btnEdit;
 
@@ -41,7 +50,7 @@ public class AirlineController {
     private AnchorPane userCrud;
 
     @FXML
-    private TableView<?> airlineTbl;
+    private TableView<Airline> airlineTbl;
 
     @FXML
     private TableColumn<?, ?> idCol;
@@ -53,7 +62,7 @@ public class AirlineController {
     private TableColumn<?, ?> logoCol;
 
     @FXML
-    private TableColumn<?, ?> actionsCol;
+    private TableColumn<Airline, String> actionsCol;
 
     private Airport airport;
     private DashboardController dController;
@@ -78,7 +87,9 @@ public class AirlineController {
 
     @FXML
     void addLogo(ActionEvent event) {
-
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Seleccionar Archivo");
+        File file = fileChooser.showOpenDialog(txtName.getScene().getWindow());
     }
 
     @FXML
@@ -88,11 +99,110 @@ public class AirlineController {
 
     @FXML
     void editAirline(ActionEvent event) {
-
+        if (validateFields()) {
+            selected.setAirlineName(txtName.getText());
+            dController.geAirportController().createAlert("Airline was successfully edited.", Route.SUCCESS);
+            airport.saveData();
+            airport.loadData();
+            getData();
+            modal.close();
+        } else {
+            dController.geAirportController().createAlert(Constant.EMPTY_FIELDS, Route.WARNING);
+        }
     }
 
     @FXML
     void saveAirline(ActionEvent event) {
+        if (validateFields()) {
+            airport.getAirlines().add(new Airline(txtName.getText(), "RUTA DEL LOGO"));
+            dController.geAirportController().createAlert("Airline was successfully added.", Route.SUCCESS);
+            airport.saveData();
+            airport.loadData();
+            getData();
+            modal.close();
+        } else {
+            dController.geAirportController().createAlert(Constant.EMPTY_FIELDS, Route.WARNING);
+        }
+    }
 
+    private void getData() {
+        ObservableList<Airline> airlines = FXCollections.observableArrayList(airport.getAirlines());
+        idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("airlineName"));
+        logoCol.setCellValueFactory(new PropertyValueFactory<>("logo"));
+        renderActions();
+        airlineTbl.setItems(airlines);
+    }
+
+    private void renderActions() {
+        Callback<TableColumn<Airline, String>, TableCell<Airline, String>> cellFact = (
+                TableColumn<Airline, String> param) -> {
+            final TableCell<Airline, String> cell = new TableCell<Airline, String>() {
+                @Override
+                public void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setGraphic(null);
+                        setText(null);
+                    } else {
+                        Button delete = new Button("Delete");
+                        delete.setId("delete");
+                        Button edit = new Button("Edit");
+                        edit.setId("edit");
+                        delete.getStylesheets().add(Route.CRUD.getRoute());
+                        edit.getStylesheets().add(Route.CRUD.getRoute());
+                        delete.setOnAction((ActionEvent event) -> {
+                            selected = (Airline) getTableRow().getItem();
+                            airport.getAirlines().remove(selected);
+                            dController.geAirportController().createAlert("Airline was deleted successfully.", Route.SUCCESS);
+                            getData();
+                        });
+                        edit.setOnAction((ActionEvent event) -> {
+                            selected = (Airline) getTableRow().getItem();
+                            try {
+                                showModal();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            modalName.setText("Edit User");
+                            prepareEdition(selected);
+                        });
+                        HBox managebtn = new HBox(edit, delete);
+                        managebtn.setStyle("-fx-alignment:center");
+                        HBox.setMargin(delete, new Insets(2, 2, 0, 3));
+                        HBox.setMargin(edit, new Insets(2, 3, 0, 2));
+                        setGraphic(managebtn);
+                        setText(null);
+                    }
+                }
+            };
+            return cell;
+        };
+        actionsCol.setCellFactory(cellFact);
+    }
+
+    public void showModal() throws IOException {
+        Stage stage = dController.loadModal(Route.AIRLINE_MODAL, this);
+        setModal(stage);
+        stage.show();
+    }
+    
+    private boolean validateFields() {
+        boolean render = true;
+        if (txtName.getText().isEmpty()) {
+            render = false;
+        }
+        return render;
+    }
+    
+    public void prepareEdition(Airline selected) {
+        txtName.setText(selected.getAirlineName());
+        btnEdit.setVisible(true);
+        btnSave.setVisible(false);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        getData();
     }
 }
